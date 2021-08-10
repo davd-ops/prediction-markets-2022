49cd4b05a82e895346a515e7df3dee6cde15331b
DavidPsencik <davidpsencik459@gmail.com>
2021-08-10 22:27:45 +0200

    betting ratio calculation

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -2,30 +2,63 @@
 pragma solidity ^0.8.0;
 
 import "./Ownable.sol";
+import "./SafeMath.sol";
 import "hardhat/console.sol";
 
 
 contract PredictionMarket is Ownable {
-    string public marketName;
-    string public firstChoice;
-    string public secondChoice;
-    uint public firstChoiceWeiBalance;
-    uint public secondChoiceWeiBalance;
-    
+    using SafeMath for uint;
 
-    constructor(string memory _name, string memory _firstChoice, string memory _secondChoice) {
+    string public marketName;
+    uint public startingBlock;
+    uint public endingBlock;
+    uint public yesWeiBalance;
+    uint public noWeiBalance;
+    uint public yesSharesEmitted;
+    uint public noSharesEmitted;
+    
+    mapping(address => uint) public yesAddressesBalanceMapping;
+    mapping(address => uint) public noAddressesBalanceMapping;
+
+    constructor(string memory _name, uint _endingBlock) {
         marketName = _name;
-        firstChoice = _firstChoice;
-        secondChoice = _secondChoice;
+        startingBlock = block.timestamp;
+        endingBlock = _endingBlock;
+        //console.log("Start: ", startingBlock);
+        //console.log("End: ", endingBlock);
     }
 
-    function betOnMarket(uint _choiceIdentifier) external payable {
-        require(_choiceIdentifier <= 2, "Incorrect choice identifier. Insert 0/1.");
+    function betOnMarket(string memory _choiceIdentifier) external payable {
+        require(keccak256(abi.encodePacked(_choiceIdentifier)) == keccak256(abi.encodePacked("yes")) || keccak256(abi.encodePacked(_choiceIdentifier)) == keccak256(abi.encodePacked("no")), "Incorrect choice. Insert yes/no.");
         require(msg.value >= 0.1 ether, "You need to bet atleast 0.1 ETH.");
+
+        if (keccak256(abi.encodePacked(_choiceIdentifier)) == keccak256(abi.encodePacked("yes"))){
+            yesWeiBalance = yesWeiBalance.add(msg.value);
+            yesAddressesBalanceMapping[msg.sender] = yesAddressesBalanceMapping[msg.sender].add(msg.value);
+            ++yesSharesEmitted;
+        } else if (keccak256(abi.encodePacked(_choiceIdentifier)) == keccak256(abi.encodePacked("no"))) {
+            noWeiBalance = noWeiBalance.add(msg.value);
+            noAddressesBalanceMapping[msg.sender] = noAddressesBalanceMapping[msg.sender].add(msg.value);
+            ++noSharesEmitted;
+        }
     }
 
     function getBalance() external view returns (uint) {
         return address(this).balance;
+    }
+
+    function getBettingRatio() public {
+        uint numberOfShares = yesSharesEmitted.add(noSharesEmitted);
+        numberOfShares = 10;
+        yesSharesEmitted = 1;
+        noSharesEmitted = 9;
+        console.log("numberof shares ", numberOfShares);
+        //uint onePercent = numberOfShares.div(100);
+        //console.log("onepercent ", onePercent);
+        uint yesRatio = yesSharesEmitted.mul(10000000).div(numberOfShares);
+        uint noRatio = noSharesEmitted.mul(10000000).div(numberOfShares);
+        console.log("yes ratio: 0, ", yesRatio);
+        console.log("no ratio: 0, ", noRatio);
     }
 
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,9 +2,7 @@ const { expect } = require("chai");
 
 describe("PredictionMarket Contract", function () {
   const marketName = "FirstMarket";
-  const firstChoice = "FirstChoice";
-  const secondChoice = "SecondChoice";
-  const choice = 0;
+  const choice = "yes";
   const valueDeposited = "0.1"; // in ether
 
 
@@ -15,14 +13,12 @@ describe("PredictionMarket Contract", function () {
 
     [creator, signer, receiver, ...address] = await ethers.getSigners();
   
-    hardhatPredictionMarket = await PredictionMarket.deploy(marketName, firstChoice, secondChoice);
+    hardhatPredictionMarket = await PredictionMarket.deploy(marketName, 46465465465456);
   });
 
   describe("Deployment", function() {
-    it("Should set up market and choice names correctly", async function() {
+    it("Should set up market name correctly", async function() {
         expect(await hardhatPredictionMarket.marketName()).to.equal(marketName);
-        expect(await hardhatPredictionMarket.firstChoice()).to.equal(firstChoice);
-        expect(await hardhatPredictionMarket.secondChoice()).to.equal(secondChoice);
     });
   });
 
@@ -31,6 +27,11 @@ describe("PredictionMarket Contract", function () {
         await hardhatPredictionMarket.betOnMarket(choice, {value: ethers.utils.parseEther(valueDeposited)});
         expect(await hardhatPredictionMarket.getBalance()).to.equal(ethers.utils.parseEther(valueDeposited));
     });
+    it("Should be able to calculate ratio", async function() {
+      await hardhatPredictionMarket.betOnMarket(choice, {value: ethers.utils.parseEther(valueDeposited)});
+      await hardhatPredictionMarket.getBettingRatio();
+      //expect(await hardhatPredictionMarket.getBalance()).to.equal(ethers.utils.parseEther(valueDeposited));
+  });
   });
 
 });
